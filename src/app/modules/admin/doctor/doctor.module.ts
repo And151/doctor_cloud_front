ef235c00a1d80ac47adc72c231d67581abb020a6
Inesa <toroyaninesa@gmail.com>
2022-04-20 17:43:37 +0400

    doctors

--- src/app/modules/admin/doctor/doctor.module.ts
+++ src/app/modules/admin/doctor/doctor.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DoctorComponent } from './doctor.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DoctorComponent} from './doctor.component';
 import {RouterModule} from "@angular/router";
 import {DoctorRouting} from "./doctor.routing";
-
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {SharedModule} from "../../../shared/shared.module";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatInputModule} from "@angular/material/input";
 
 
 @NgModule({
-  declarations: [
-    DoctorComponent
-  ],
-  imports: [
-    CommonModule,
-      RouterModule.forChild(DoctorRouting)
-  ]
+    declarations: [
+        DoctorComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(DoctorRouting),
+        MatFormFieldModule,
+        FormsModule,
+        SharedModule,
+        MatDividerModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatInputModule
+    ]
 })
-export class DoctorModule { }
+export class DoctorModule {
+}
--- src/app/service/doctor.service.ts
+++ src/app/service/doctor.service.ts
@@ -3,12 +3,16 @@ import {HttpClient} from "@angular/common/http";
 import {Location} from "@angular/common";
 import {environment} from "../../environments/environment";
 import {IUser} from "../models/user.model";
+import {BehaviorSubject} from "rxjs";
+import {IHospital} from "../models/hospitals.model";
+import {tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorService {
 
+  doctors$ = new BehaviorSubject<IUser[]>([]);
   constructor(
       private _http: HttpClient
   ) { }
@@ -17,9 +21,8 @@ export class DoctorService {
     const url = Location.joinWithSlash(
         environment.origin || '', '/user/doctors'
     )
-    return this._http.get<IUser[]>(url);
-
+    return this._http.get<IUser[]>(url).pipe(
+        tap(item => this.doctors$.next(item))
+    );
   }
-
-
 }
